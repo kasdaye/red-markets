884ecb1824cc6f9972a9ac7b94d07cf5c2955591
sam-bandyopadhyay <rwijub@gmail.com>
2021-06-27 20:19:44 -0600

    Allow modifiers to rolls and editting owned items

--- module/sheets/character-sheet.js
+++ module/sheets/character-sheet.js
@@ -13,6 +13,7 @@ export default class CharacterSheet extends ActorSheet {
     activateListeners(html) {
         if (this.actor.isOwner) {
             html.find(".skill-roll").click(this._onSkillRoll.bind(this));
+            html.find(".item-edit").click(this._onItemEdit.bind(this));
         }
 
         super.activateListeners(html);
@@ -27,22 +28,56 @@ export default class CharacterSheet extends ActorSheet {
     }
 
     _onSkillRoll(event) {
-        let rollFormula = "1d10 + @actionValue - 1d10";
-        let rollData = {
-            actionValue: event.currentTarget.dataset.actionValue
-        };
-        let roll = new Roll(rollFormula, rollData).roll();
-        let success = roll.total > 0;
-
-        console.log(roll);
-
         let actionName = event.currentTarget.dataset.actionName;
-        const promises = [roll.toMessage({
-            flavor: success ? "Success on " + actionName + "!" : "Failure on " + actionName + "!",
-            user: game.user.data._id,
-            speaker: ChatMessage.getSpeaker()
-        })];
+        let confirmed = false;
 
-        return Promise.all(promises).then(() => roll);
+        new Dialog({
+            title: "Roll " + actionName,
+            content: `
+             <form>
+              <div class="form-group">
+               <label>Roll Modifier:</label>
+               <input id="modifier-value" name="modifier-value" value="0"></input>
+              </div>
+             </form>
+             `,
+            buttons: {
+                one: {
+                    icon: '<i class="fas fa-check"></i>',
+                    label: "Roll!",
+                    callback: () => confirmed = true
+                },
+                two: {
+                    icon: '<i class="fas fa-times"></i>',
+                    label: "Cancel",
+                    callback: () => confirmed = false
+                }
+            },
+            default: "Cancel",
+            close: html => {
+                if (confirmed) {
+                    let rollFormula = "1d10 + @skillModifier + @chargeModifier - 1d10";
+                    let rollData = {
+                        skillModifier: event.currentTarget.dataset.actionValue,
+                        chargeModifier: parseInt(html.find('[name=modifier-value]')[0].value)
+                    };
+                    var roll = new Roll(rollFormula, rollData).roll();
+                    let success = roll.total > 0;
+                    roll.toMessage({
+                        flavor: success ? "Success on " + actionName + "!" : "Failure on " + actionName + "!",
+                        user: game.user.data._id,
+                        speaker: ChatMessage.getSpeaker()
+                    });
+                }
+            }
+        }).render(true);
+    }
+
+    _onItemEdit(event) {
+        event.preventDefault();
+        let element = event.currentTarget;
+        let itemId = element.closest(".actor-item").dataset.itemId;
+        let item = this.actor.items.get(itemId);
+        item.sheet.render(true);
     }
 }
